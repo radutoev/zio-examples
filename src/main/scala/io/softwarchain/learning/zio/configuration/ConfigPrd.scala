5cd7d7d0097839c8db315c562c777a277977f75f
Radu Toev <radu.toev@softwarechain.io>
2020-03-26 10:22:31 +0200

    Remove legacy config

--- src/main/scala/io/softwarchain/learning/zio/configuration/ConfigPrd.scala
+++ src/main/scala/io/softwarchain/learning/zio/configuration/ConfigPrd.scala
@@ -1,12 +1,12 @@
 package io.softwarchain.learning.zio.configuration
 
-import pureconfig.loadConfigOrThrow
-import zio.{Has, Layer, Task, ZLayer}
+import pureconfig.ConfigSource
 import pureconfig.generic.auto._
+import zio.{Has, Layer, Task, ZLayer}
 
 object ConfigPrd {
   val live: Layer[Throwable, Configuration] = ZLayer.fromEffectMany(
     Task
-      .effect(loadConfigOrThrow[AppConfig])
-      .map(c => Has(c.api) ++ Has(c.dbConfig)))
+      .effect(ConfigSource.default.loadOrThrow[AppConfig])
+      .map(c => Has(c.api))) // ++ Has(c.dbConfig)
 }
--- src/main/scala/io/softwarchain/learning/zio/configuration/package.scala
+++ src/main/scala/io/softwarchain/learning/zio/configuration/package.scala
@@ -4,16 +4,10 @@ import zio.{Has, ZIO}
 
 package object configuration {
 
-  type Configuration = Has[ApiConfig] with Has[DbConfig]
+  type Configuration = Has[ApiConfig]
 
-  case class AppConfig(api: ApiConfig, dbConfig: DbConfig)
+  case class AppConfig(api: ApiConfig)
   case class ApiConfig(endpoint: String, port: Int)
-  case class DbConfig(
-      url: String,
-      user: String,
-      password: String
-  )
 
   val apiConfig: ZIO[Has[ApiConfig], Throwable, ApiConfig] = ZIO.access(_.get)
-  val dbConfig: ZIO[Has[DbConfig], Throwable, DbConfig] = ZIO.access(_.get)
 }
